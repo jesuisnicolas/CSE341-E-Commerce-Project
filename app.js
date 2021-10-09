893c75d8f3613fe575b5cb971e1ed140d7ccb69a
Nico Scha <64054331+jesuisnicolas@users.noreply.github.com>
2021-10-09 01:58:30 -0300

    fixed user id

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public"))); //this allows us to acc
 
 
 app.use((req, res, next) => {
-  User.findById("6160e67680a35e09825d06bc")
+  User.findById("61611dde86f074315481bf00")
     .then(user => {
       req.user = user;
       next();
